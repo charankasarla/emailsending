d4cd6fa0795d88f05014c174abcb268e3f8860e3
shivasaicharankasarla <shivasaicharankasarla03@gmail.com>
2023-10-09 20:54:08 +0530

    small Changes

--- FirstProject/FirstProject/settings.py
+++ FirstProject/FirstProject/settings.py
@@ -125,8 +125,3 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_USE_TLS = True
-#EMAIL_PORT = 587
-#EMAIL_HOST_USER = "shivasaicharankasarla03@gmail.com"
-#EMAIL_HOST_PASSWORD = "Charan1156"
